cd12ece65c5afb9eb7f95283d09546a4e7a13972
Batya Z <batya21194@gmail.com>
2025-01-22 01:25:00 +0200

    first commit

---
+++ practicode2/Program.cs
@@ -0,0 +1,15 @@
+﻿using practicode2;
+
+var serializer = new HtmlSerializer();
+
+var html = await serializer.Load("https://claude.ai");
+
+var rootElement = serializer.Serialize(html);
+
+var selector = Selector.Parse("div.content #main");
+var elements = rootElement.FindElements(selector);
+
+foreach (var descendant in rootElement.Descendants())
+{
+    Console.WriteLine(descendant.Name);
+}
